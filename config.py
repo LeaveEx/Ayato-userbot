df39a09c231d920e6944ff30e652f25fd2778549
LeaveEx <126212388+LeaveEx@users.noreply.github.com>
2023-02-28 11:01:34 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,57 @@
+# Thanks For: @mrismanaziz
+# Copyright (C) 2022 Pyro-ManUserbot
+#
+# This file is a part of < https://github.com/LeaveEx/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/mrismanaziz/PyroMan-Userbot/blob/main/LICENSE/>.
+#
+# t.me/SharingUserbot & t.me/ramsupportt
+
+
+from distutils.util import strtobool
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
+
+ALIVE_EMOJI = getenv("ALIVE_EMOJI", "🐻‍❄️")
+ALIVE_LOGO = getenv("ALIVE_LOGO", "https://telegra.ph/file/da0f79694e2c3d44bdfa3.jpg")
+ALIVE_TEKS_CUSTOM = getenv("ALIVE_TEKS_CUSTOM", "Hey bro, I am rams.")
+API_HASH = getenv("API_HASH", "551d747d492ad11a10054fbf672d16e3")
+API_ID = getenv("API_ID", "3330416")
+BLACKLIST_CHAT = getenv("BLACKLIST_CHAT", None)
+if not BLACKLIST_CHAT:
+    BLACKLIST_CHAT = [1680004937]
+BLACKLIST_GCAST = {int(x) for x in getenv("BLACKLIST_GCAST", "").split()}
+BOTLOG_CHATID = int(getenv("BOTLOG_CHATID") or 0)
+BOT_TOKEN = getenv("BOT_TOKEN", None)
+BOT_VER = "1.0.7"
+BRANCH = getenv("BRANCH", "main")
+CH_SFS = getenv("CH_SFS", "b4c0d")
+IG_ALIVE = getenv("IG_ALIVE", "mzxfaiz.xz")
+CHANNEL = getenv("CHANNEL", "idealizerd")
+CMD_HANDLER = getenv("CMD_HANDLER", ".")
+CMD_HNDLR = CMD_HANDLER
+ID_OWNER = getenv("ID_OWNER", "1680004937")
+DB_URL = getenv("DATABASE_URL", "")
+GIT_TOKEN = getenv("GIT_TOKEN", "")
+GROUP = getenv("GROUP", "senzusupp")
+HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
+PMPERMIT_PIC = getenv("PMPERMIT_PIC", None)
+OPENAI_API_KEY = getenv("OPENAI_API_KEY", "")
+PM_AUTO_BAN = strtobool(getenv("PM_AUTO_BAN", "True"))
+REPO_URL = getenv("REPO_URL", "https://github.com/LeaveEx/Ayato-userbot")
+STRING_SESSION1 = getenv("STRING_SESSION1", "")
+STRING_SESSION2 = getenv("STRING_SESSION2", "")
+STRING_SESSION3 = getenv("STRING_SESSION3", "")
+STRING_SESSION4 = getenv("STRING_SESSION4", "")
+STRING_SESSION5 = getenv("STRING_SESSION5", "")
+STRING_SESSION6 = getenv("STRING_SESSION6", "")
+STRING_SESSION7 = getenv("STRING_SESSION7", "")
+STRING_SESSION8 = getenv("STRING_SESSION8", "")
+STRING_SESSION9 = getenv("STRING_SESSION9", "")
+STRING_SESSION10 = getenv("STRING_SESSION10", "")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
