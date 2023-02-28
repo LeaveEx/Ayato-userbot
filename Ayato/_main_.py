e35e54006743e80dd789623eb17307ca1e96da8c
LeaveEx <126212388+LeaveEx@users.noreply.github.com>
2023-02-28 07:24:43 +0700

    Update _main_.py

--- Ayato/_main_.py
+++ Ayato/_main_.py
@@ -1,26 +1,54 @@
-import asyncio
-import importlib
-from pyrogram import Client, idle
-from Ayato.helper import join
+import time
+import importlib 
+from pyrogram import idle
+from uvloop import install
+
+from config import BOT_VER, CMD_HANDLER
+from Ayato import BOTLOG_CHATID, LOGGER, LOOP, aiosession, bot1, bots, app, ids
+from Ayato.split.misc import create_botlog, git, heroku
 from Ayato.modules import ALL_MODULES
-from Ayato import clients, app, ids
 
-async def start_bot():
+MSG_ON = """
+🐻‍❄️ **KAMPANG-UserBot Menyala** 🐻‍❄️
+━───────╯⇕╰───────━
+🐻‍❄️ **Userbot Version -** `{}`
+🐻‍❄️ prefixes: ? ! , . *
+🐻‍❄️ **Ketik** `{}KAMPANG` **untuk Mengecheck Bot**
+━───────╮⇕╭───────━
+"""
+
+async def main():
     await app.start()
-    print("LOG: Founded Bot token Booting..")
     for all_module in ALL_MODULES:
-        importlib.import_module("Ayato.modules" + all_module)
-        print(f"Successfully Imported {all_module} 💥")
-    for cli in clients:
+        importlib.import_module(f"Ayato.modules.{all_module}")
+    for bot in bots:
         try:
-            await cli.start()
-            ex = await cli.get_me()
-            await join(cli)
-            print(f"Started {ex.first_name} 🔥")
-            ids.append(ex.id)
-        except Exception as e:
-            print(f"{e}")
+            await bot.start()
+            bot.me = await bot.get_me()
+            await bot.join_chat("senzusupp")
+            await bot.join_chat("idealizerd")
+            await bot.join_chat("themusicLd")
+            ids.append(bot.me.id)
+            try:
+                await bot.send_message(
+                    BOTLOG_CHATID, MSG_ON.format(BOT_VER, CMD_HANDLER)
+                )
+            except BaseException:
+                pass
+            LOGGER("Ayato").info(
+                f"Logged in as {bot.me.first_name} | [ {bot.me.id} ]"
+            )
+        except Exception as a:
+            LOGGER("main").warning(a)
+    LOGGER("Ayato").info(f"KAMOANG-UserBot v{BOT_VER} [🐻‍❄️ MENYALA YA ANJENGGG! 🐻‍❄️]")
+    if not str(BOTLOG_CHATID).startswith("-100"):
+        await create_botlog(bot1)
     await idle()
+    await aiosession.close()
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(start_bot())
+
+if __name__ == "__main__":
+    LOGGER("Ayato").info("Starting KAMOANG-UsserBot")
+    install()
+    heroku()
+    LOOP.run_until_complete(main())
