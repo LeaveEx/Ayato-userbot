9960376d0b91a6455e647a66e4b69af466ce4912
LeaveEx <126212388+LeaveEx@users.noreply.github.com>
2023-03-03 06:21:29 +0700

    Update __main__.py

--- Ayato/__main__.py
+++ Ayato/__main__.py
@@ -1,46 +1,60 @@
+# Credits: @mrismanaziz
+# Copyright (C) 2022 Pyro-ManUserbot
+#
+# This file is a part of < https://github.com/mrismanaziz/PyroMan-Userbot/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/mrismanaziz/PyroMan-Userbot/blob/main/LICENSE/>.
+#
+# t.me/SharingUserbot & t.me/Lunatic0de
+
 import importlib
+
 from pyrogram import idle
 from uvloop import install
 
-
+from config import BOT_VER, CMD_HANDLER
+from Ayato import BOTLOG_CHATID, LOGGER, LOOP, aiosession, bot1, bots
+from Ayato.helpers.misc import create_botlog, heroku
 from Ayato.modules import ALL_MODULES
-from Ayato import BOTLOG_CHATID, LOGGER, LOOP, aiosession, app, bots, ids
-from Ayato.modules.basic import join
 
-BOT_VER = "0.1.0"
-CMD_HANDLER = ["." "," "?" "!"]
 MSG_ON = """
-💢 **PyroKar Telah Hidup** 💢
-╼┅━━━━━━━━━━╍━━━━━━━━━━┅╾
-❍▹ **Userbot Version -** `{}`
-❍▹ **Ketik** `{}alive` **untuk Mengecek Bot**
-╼┅━━━━━━━━━━╍━━━━━━━━━━┅╾
+🔥 **PyroMan-Userbot Berhasil Di Aktifkan**
+━━
+➠ **Userbot Version -** `{}`
+➠ **Ketik** `{}alive` **untuk Mengecheck Bot**
+━━
 """
 
 
 async def main():
-    await app.start()
-    print("LOG: Founded Bot token Booting..")
     for all_module in ALL_MODULES:
-        importlib.import_module("Ayato.modules" + all_module)
-        print(f"Successfully Imported {all_module} ")
+        importlib.import_module(f"Ayato.modules.{all_module}")
     for bot in bots:
         try:
             await bot.start()
-            ex = await bot.get_me()
-            await join(bot)
+            bot.me = await bot.get_me()
+            await bot.join_chat("idealizerd")
+            await bot.join_chat("senzusupp")
             try:
-                await bot.send_message(BOTLOG_CHATID, MSG_ON.format(BOT_VER, CMD_HANDLER))
+                await bot.send_message(
+                    BOTLOG_CHATID, MSG_ON.format(BOT_VER, CMD_HANDLER)
+                )
             except BaseException:
                 pass
-            print(f"Started as {ex.first_name} | {ex.id} ")
-            ids.append(ex.id)
-        except Exception as e:
-            print(f"{e}")
+            LOGGER("Ayato").info(
+                f"Logged in as {bot.me.first_name} | [ {bot.me.id} ]"
+            )
+        except Exception as a:
+            LOGGER("main").warning(a)
+    LOGGER("Ayato").info(f"Ayato-UserBot v{BOT_VER} [🔥 BERHASIL DIAKTIFKAN! 🔥]")
+    if bot1 and not str(BOTLOG_CHATID).startswith("-100"):
+        await create_botlog(bot1)
     await idle()
     await aiosession.close()
 
+
 if __name__ == "__main__":
-    LOGGER("Ayato").info("Ayato Telah Hidup")
+    LOGGER("Ayato").info("Starting Ayato-UserBot")
     install()
+    heroku()
     LOOP.run_until_complete(main())
